c089f04ec8a626658f06361d717c1aba810dce77
x1a0Y4NGren <yangchengde1123@163.com>
2026-05-14 19:28:52 +0800

    chore: scaffold Flutter app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PlaceholderApp());
+}
+
+class PlaceholderApp extends StatelessWidget {
+  const PlaceholderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('HoopTrace'),
+        ),
+      ),
+    );
+  }
+}
