540c0bb0b73f8b5a7591bb151f33d71f0f484e15
x1a0Y4NGren <yangchengde1123@163.com>
2026-07-09 15:30:50 +0800

    fix: track generated localizations

---
+++ lib/app/l10n/app_localizations_en.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appName => 'HoopTrace';
+
+  @override
+  String get startScoring => 'Start';
+
+  @override
+  String get replayHistory => 'Replay History';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get projectDetails => 'Project';
+}
---
+++ lib/app/l10n/app_localizations_zh.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Chinese (`zh`).
+class AppLocalizationsZh extends AppLocalizations {
+  AppLocalizationsZh([String locale = 'zh']) : super(locale);
+
+  @override
+  String get appName => 'HoopTrace';
+
+  @override
+  String get startScoring => '开始计分';
+
+  @override
+  String get replayHistory => '复盘历史';
+
+  @override
+  String get settings => '设置';
+
+  @override
+  String get projectDetails => '项目详情';
+}
